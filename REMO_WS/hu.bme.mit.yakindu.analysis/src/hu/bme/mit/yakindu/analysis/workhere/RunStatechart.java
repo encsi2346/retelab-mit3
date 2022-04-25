87786310998a019cebf9450c5f505452060ba979
encsi2346 <53266928+encsi2346@users.noreply.github.com>
2022-04-25 15:18:27 +0200

    3.feladat

--- REMO_WS/hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
+++ REMO_WS/hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
@@ -1,23 +1,28 @@
 package hu.bme.mit.yakindu.analysis.workhere;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 // import hu.bme.mit.yakindu.analysis.RuntimeService;
 // import hu.bme.mit.yakindu.analysis.TimerService;
 import hu.bme.mit.yakindu.analysis.example.ExampleStatemachine;
 import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
 
-
+import java.io.IOException;
+import hu.bme.mit.yakindu.analysis.RuntimeService;
+import hu.bme.mit.yakindu.analysis.TimerService;
+import hu.bme.mit.yakindu.analysis.example.ExampleStatemachine;
+import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
 
 public class RunStatechart {
 	
 	public static void main(String[] args) throws IOException {
-//		ExampleStatemachine s = new ExampleStatemachine();
-//		s.setTimer(new TimerService());
-//		RuntimeService.getInstance().registerStatemachine(s, 200);
-//		s.init();
-//		s.enter();
-//		s.runCycle();
+		ExampleStatemachine s = new ExampleStatemachine();
+		s.setTimer(new TimerService());
+		RuntimeService.getInstance().registerStatemachine(s, 200);
+		s.init();
+		s.enter();
+		s.runCycle();
 //		print(s);
 //		s.raiseStart();
 //		s.runCycle();
@@ -26,6 +31,35 @@ public class RunStatechart {
 //		s.runCycle();
 //		print(s);
 //		System.exit(0);
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			String line = scanner.next();
+			switch(line) {
+				case "start": 
+					s.raiseStart();
+					s.runCycle();
+					print(s);
+					break;
+				case "white": 
+					s.raiseWhite();
+					s.runCycle();
+					print(s);
+					break;
+				case "black": 
+					s.raiseBlack();
+					s.runCycle();
+					print(s);
+					break;
+				case "exit": 
+					scanner.close();
+					System.exit(0);
+					break;
+				default: break;
+			}
+			
+		}
+		
+			
 	}
 
 	public static void print(IExampleStatemachine s) {
